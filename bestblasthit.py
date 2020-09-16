6567656f8e78ecece0984365769d22a60f74b30e
deprekate <deprekate@gmail.com>
2020-09-16 14:54:18 -0700

    Added more functionality

--- bestblasthit.py
+++ bestblasthit.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
-
+import os
 import sys
 import fileinput
+import argparse
+from argparse import RawTextHelpFormatter
+
+
+def is_valid_file(x):
+	if not os.path.exists(x):
+		raise argparse.ArgumentTypeError("{0} does not exist".format(x))
+	return open(x)
+
+usage = 'bestblasthit.py [-opt1, [-opt2, ...]] infile'
+parser = argparse.ArgumentParser(description='a blastn output formatter', formatter_class=RawTextHelpFormatter, usage=usage)
+parser.add_argument('-i', '--input',  type=is_valid_file, default=sys.stdin, help='input file in typical blasttab format')
+parser.add_argument('-a', '--agg', type=int, action="store", default=0, dest='agg', help='number of the column to aggregate on')
+parser.add_argument('-s', '--skip', action="store_true", dest='skip', help='whether to skip ambiguous hits')
+args = parser.parse_args()
 
 
 class ReadHit():
 	def __init__(self):
+		# index is the column to use as measure for score: 10 is evalue
+		self.index = 10
 		self.read_line = ''
 		self.read_name = None
 		self.read_score = float('+Inf')
-		self.reads = list(' ')
-		self.index = 10
+		self.reads = list()
 
 	def __str__(self):
 		return next(iter(self.reads or []), None)
@@ -33,18 +49,41 @@ class ReadHit():
 		self.read_line = line.rstrip('\n')
 		self.read_name = column[0]
 		self.read_score = float(column[self.index])
-		self.names = list()
+		self.reads = list()
+		self.reads.append(line)
 
+
+taxa_counts = dict()
+def print_or_parse(rh):
+	counts = dict()
+	for read in rh.reads:
+		if args.agg:
+			column = read.rstrip('\n').split('\t')
+			counts[column[args.agg]] = counts.get(column[args.agg], 0) + 1/len(rh.reads)
+		else:
+			print(read, end='')
+	if not args.skip or len(counts)==1:
+		for key,value in counts.items():
+			taxa_counts[key] = taxa_counts.get(key, 0) + value
 
 
 read_hit = ReadHit()
+for line in args.input:
 
-for line in fileinput.input():
+	# skip comment lines or hits less than 1e-5
 	column = line.rstrip('\n').split('\t')
-	if float(column[10]) > 0.00001:
+	if line.startswith('#') or float(column[10]) > 0.00001:
 		continue
+
+	# add a hit to the read object: returns false if hit belongs to a new read
 	if not read_hit.add(line):
-		print(read_hit)
+		print_or_parse(read_hit)
 		read_hit.new(line)
 
+#you have to do it one last time
+print_or_parse(read_hit)
 
+
+if args.agg:
+	for key,value in taxa_counts.items():
+		print(key, value, sep='\t')
